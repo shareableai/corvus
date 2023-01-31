e09c38b863eec25de8d4aa29555bfa1b1bd201a7
Lissa Hyacinth <lissahyacinth@gmail.com>
2023-01-31 23:55:09 +0000

    Rename model_id -> artefact schema id for consistency with Jackdaw.

--- corvus/model_output.py
+++ corvus/model_output.py
@@ -20,10 +20,10 @@ def models_to_json(models: Iterable[PyModelSearchResult]) -> None:
             repository = model.vcs_info.remote_repository.repository
         model_dicts.append({
             "model_name": model.model_id.name,
-            "short_model_id": str(model.model_id.short_schema_id),
+            "short_artefact_schema_id": str(model.model_id.short_schema_id),
             "repository": f"{owner}\\{repository}" if owner is not None else "No Remote VCS Configured",
             "branch": model.vcs_info.branch,
-            "git_sha": model.vcs_info.sha[0:7],
+            "short_vcs": model.vcs_info.sha[0:7],
             "size": sizeof_fmt(model.model_id.model_size),
             "creation_time": datetime.datetime.fromtimestamp(
                 model.creation_time
@@ -37,10 +37,10 @@ def models_to_table(models: Iterable[PyModelSearchResult]) -> None:
     console.print()
     table = Table(show_header=True, header_style="cyan")
     table.add_column("Model Name")
-    table.add_column("Short Model ID", overflow="ignore")
+    table.add_column("Short Artefact Schema ID", overflow="ignore")
     table.add_column("Repository")
     table.add_column("Branch")
-    table.add_column("Git SHA")
+    table.add_column("Short VCS")
     table.add_column("Size", justify="right")
     table.add_column("Creation Time")
     for model in models:
